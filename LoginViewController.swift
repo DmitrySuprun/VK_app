9ffe23052bd44ab492ca8d645f6d6a6832942f6e
DmitrySuprun <suprun1212@gmail.com>
2022-04-06 20:31:49 +0300

    1ex keyboardfix

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -8,8 +8,30 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // MARK: - Public Properties
+    // MARK: - Private Properties
+    // MARK: - Initializers
+    
+    // MARK: - Override Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    // MARK: - IBActions
+
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
+    // MARK: - Public Methods
+    // MARK: - Private Methods
+
 }
+
