c10a6527c408820abe6d4435ba91a7a689ed8c34
DmitrySuprun <suprun1212@gmail.com>
2022-04-20 06:30:21 +0300

    Ex 5.3 final

--- VK/News/NewsTableViewCell.swift
+++ VK/News/NewsTableViewCell.swift
@@ -12,9 +12,11 @@ class NewsTableViewCell: UITableViewCell {
     @IBOutlet weak var imageNews: UIImageView!
     @IBOutlet weak var newsText: UILabel!
     
+    @IBOutlet weak var likeButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        newsText.text = "Создать экран новостей. Добавить туда таблицу и сделать ячейку для новости.  Ячейка должна содержать то же самое, что и в оригинальном приложении ВКонтакте: надпись, фотографии, кнопки «Мне нравится», «Комментировать», «Поделиться» и индикатор количества просмотров. Сделать поддержку только одной фотографии, которая должна быть квадратной формы и растягиваться на всю ширину ячейки. Высота ячейки должна вычисляться автоматически."
+        newsText.text = "Создать экран новостей.\n Добавить туда таблицу и сделать ячейку для новости.\n  Ячейка должна содержать то же самое, что и в оригинальном приложении ВКонтакте: надпись, фотографии, кнопки «Мне нравится», «Комментировать», «Поделиться» и индикатор количества просмотров. Сделать поддержку только одной фотографии, которая должна быть квадратной формы и растягиваться на всю ширину ячейки. Высота ячейки должна вычисляться автоматически."
         imageNews.layer.cornerRadius = 20
     }
 
@@ -22,6 +24,24 @@ class NewsTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    @IBAction func showMoreButton(_ sender: Any) {
+        newsText.numberOfLines = 0
+        let tableView = superview as! UITableView
+        tableView.reloadData()
+    }
+    
+    @IBAction func likeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func commentButton(_ sender: Any) {
+    }
+    
+    @IBAction func forwardButton(_ sender: Any) {
+    }
+    
+    @IBAction func viewsButton(_ sender: Any) {
     }
     
 }
